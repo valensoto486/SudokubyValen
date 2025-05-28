0cb4cf79dbf4151a5e300c9192959caa5d934696
valensoto486 <valensoto486@gmail.com>
2025-05-28 01:21:02 -0500

    feat: configuracion capacitor

---
+++ sudoku/android/app/src/main/java/com/valen/sudoku/MainActivity.java
@@ -0,0 +1,5 @@
+package com.valen.sudoku;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ sudoku/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.valen.sudoku',
+  appName: 'SudokubyValen',
+  webDir: 'build'
+};
+
+export default config;
